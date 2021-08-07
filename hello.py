bf927d0d22ec506efe379217f7ccafaf27a97cea
Gene Newman <eugene.newman@verizon.net>
2021-08-07 13:48:12 +0000

    Added new function sayHello()

--- hello.py
+++ hello.py
@@ -3,5 +3,8 @@ def add(x, y):
 
     return x + y
 
+def sayHello():
+    return "Hello"
 
+print(sayHello())
 print(add(1, 1))
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,7 @@
-from hello import add
+from hello import add, sayHello
 
+def test_sayHello():
+    assert "Hello" == sayHello()
 
 def test_add():
     assert 2 == add(1, 1)
